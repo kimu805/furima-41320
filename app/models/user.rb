492ea8b342a1394fe9f031d06aadc7aef5b41581
kimu805 <kimukimuok805@gmail.com>
2024-07-21 18:04:58 +0900

    add user management

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :last_name_kanji, presence: true
+  validates :first_name_kanji, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字（漢字・ひらがな・カタカナ）を使用してください' } do
+    validates :last_name_kanji
+    validates :first_name_kanji
+  end
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字（カタカナ）を使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  validates :birthday, presence: true
 end
