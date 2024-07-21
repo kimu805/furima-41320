fd6359dbf6e55268bb8dffa0d350318526c8458d
kimu805 <kimukimuok805@gmail.com>
2024-07-21 19:13:45 +0900

    implement model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :last_name_kanji, presence: true
-  validates :first_name_kanji, presence: true
+  validates :password, format: {
+    with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,
+    message: 'は半角英数字の混合である必要があります'
+  }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字（漢字・ひらがな・カタカナ）を使用してください' } do
     validates :last_name_kanji
     validates :first_name_kanji
   end
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字（カタカナ）を使用してください' } do
     validates :last_name_kana
     validates :first_name_kana
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
+    nickname              {"鈴木まさし"}
     email                 {Faker::Internet.email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    last_name_kanji       {"鈴木"}
+    first_name_kanji      {"まさし"}
+    last_name_kana        {"スズキ"}
+    first_name_kana       {"マサシ"}
+    birthday              {Faker::Date.between(from: 2.days.ago, to: Date.today)}
   end
 end
