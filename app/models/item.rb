bd60237b604ea7f6fea92a19b66d82ad486da1b2
kimu805 <kimukimuok805@gmail.com>
2024-07-22 16:56:35 +0900

    add active hash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_pay
+  belongs_to :prefecture
+  belongs_to :shipping_time
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_pay_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shippint_time_id, numericality: { other_than: 1, message: "can't be blank"}
+
 end
