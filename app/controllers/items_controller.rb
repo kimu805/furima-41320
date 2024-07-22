aa28aae9ad06b8d4c1edded90ad287a0e27832c7
kimu805 <kimukimuok805@gmail.com>
2024-07-22 14:57:42 +0900

    add action of "new" and "create" at items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_pay_id, :prefecture_id, :shipping_time_id, :price)
+  end
 end
