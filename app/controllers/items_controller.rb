6ebb8279e38bdc9b73bf72c87a7360cd6847155a
kimu805 <kimukimuok805@gmail.com>
2024-07-24 14:40:01 +0900

    add show action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_pay_id, :prefecture_id, :shipping_time_id, :price).merge(user_id: current_user.id)
