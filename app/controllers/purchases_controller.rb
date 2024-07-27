8985bf0b15cc3566c6e95d17b8b3789d2eda44f5
kimu805 <kimukimuok805@gmail.com>
2024-07-27 16:27:43 +0900

    add item instance

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,10 +1,12 @@
 class PurchasesController < ApplicationController
   def index
     @purchase_delivery = PurchaseDelivery.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
     @purchase_delivery = PurchaseDelivery.new(purchase_params)
+    @item = Item.find(params[:item_id])
     if @purchase_delivery.valid?
       @purchase_delivery.save
       redirect_to root_path
