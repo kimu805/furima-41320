5fc454dc9a6f8421f0812e12954486097e519454
kimu805 <kimukimuok805@gmail.com>
2024-07-27 16:13:10 +0900

    add action of "index" & "create" in purchases_controller

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,20 @@
 class PurchasesController < ApplicationController
   def index
+    @purchase_delivery = PurchaseDelivery.new
   end
 
   def create
+    @purchase_delivery = PurchaseDelivery.new(purchase_params)
+    if @purchase_delivery.valid?
+      @purchase_delivery.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase_delivery).permit(:post_code, :prefecture_id, :municipal, :street, :building, :call).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
