1d4c001a6557a99a3b104c52d405f93ce4aa786a
kimu805 <kimukimuok805@gmail.com>
2024-08-23 17:58:14 +0900

    add js.erb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,25 @@
 class LikesController < ApplicationController
+  before_action :set_item
 
   def create
     like = current_user.likes.build(item_id: params[:item_id])
     like.save
-    redirect_to item_path(params[:item_id])
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
     like = Like.find_by(item_id: params[:item_id], user_id: current_user.id)
     like.destroy
-    redirect_to item_path(params[:item_id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
