ae24bddb6e630d6dc8e41da1afc9c936140481be
kimu805 <kimukimuok805@gmail.com>
2024-08-23 19:11:40 +0900

    implement comment instant update

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   has_many_attached :images
   has_one :purchase
   has_many :likes
+  has_many :comments
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :items do
     resources :purchases, only: [:index, :create]
     resource :likes, only: [:create, :destroy]
+    resources :comments, only: :create
     collection do
       get "search"
     end
